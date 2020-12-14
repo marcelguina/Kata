ddb7aae3348324af7bba709d12e615948e1e2e89
Marcel Guina <marcel.guina@gmail.com>
2020-12-14 15:15:12 +0100

    C++ project created

---
+++ Kata_CodeWars_6kyu_RomanNumeralsEncoder/Kata_CodeWars_6kyu_RomanNumeralsEncoder/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Kata_CodeWars_6kyu_RomanNumeralsEncoder
+//
+//  Created by Marcel on 14/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
